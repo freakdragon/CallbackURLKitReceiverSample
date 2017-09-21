d7104d20469ab16a8d787303ef2d63842e17716f
freakdragon <freakdragon@inbox.ru>
2017-09-21 10:29:34 +0300

    Update CallbackURLKit.swift

--- CallbackURLKitReceiverSample/CallbackURLKit.swift
+++ CallbackURLKitReceiverSample/CallbackURLKit.swift
@@ -1,9 +1,9 @@
 //
 //  CallbackURLKit.swift
-//  CallbackURLKit
+//  CallbackURLKitReceiverSample
 /*
 The MIT License (MIT)
-Copyright (c) 2016 Eric Marchand (phimage)
+Copyright © 2017 Eugene Babich (freakdragon). 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
